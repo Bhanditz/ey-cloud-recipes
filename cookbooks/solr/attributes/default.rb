98986a239166348a6e1b21be8cac5a795b27849f
Richard Doe <richard.doe@rwdit.net>
2014-08-12 15:58:46 +0100

    Upgrade Solr to version 4.9.0

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -1,5 +1,5 @@
 # Set the version of Solr to install
-default[:solr_version] = "3.6.2"
+default[:solr_version] = "4.9.0"
 
 # Set the name of utility instance on which to run Solr.
 # Leave blank to install on all utility and solo instances.
