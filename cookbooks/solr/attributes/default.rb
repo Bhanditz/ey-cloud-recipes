d6c689af572bced3de6624146d2ea5f487670dff
Richard Doe <richard.doe@rwdit.net>
2016-08-01 13:13:56 +0100

    Revert Solr utility instance name

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -3,7 +3,7 @@ default[:solr_version] = "4.10.0"
 
 # Set the name of utility instance on which to run Solr.
 # Leave blank to install on all utility and solo instances.
-default[:solr_utility_name] = "solr4_m3"
+default[:solr_utility_name] = "solr4"
 
 # Set to true to configure Sunspot for Rails applications on this environment
 default[:solr_sunspot] = true
